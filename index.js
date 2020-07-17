0cbc238eb5afdf4b5341975ef817d91b477c2203
AndriiPetrovskiy <loulla2486@gmail.com>
2020-07-17 23:13:30 +0300

    DONE

---
+++ index.js
@@ -0,0 +1,41 @@
+import gallery from './gallery-items.js';
+    
+const lightbox = document.querySelector('.js-lightbox');
+const ligthImage = document.querySelector('.lightbox__image');
+const closeModal = document.querySelector('[data-action="close-lightbox"]');
+const listGalleryRef = document.querySelector('.js-gallery');
+
+
+
+function getElementByGallery(array, place) {
+    for(let item of array) {
+        const liRef = document.createElement('li');
+        const aRef = document.createElement('a');
+        const imgRef = document.createElement('img');
+        
+        imgRef.setAttribute('src', item.preview);
+        imgRef.setAttribute('data-source', item.original);
+        imgRef.setAttribute('alt', item.description);
+
+        liRef.append(aRef);
+        aRef.append(imgRef);
+        place.append(liRef);
+    };
+};
+
+const getOpenModale = (event) => {
+    if (event.target.nodeName === 'IMG') {
+        lightbox.classList.add('is-open');
+        ligthImage.src = event.target.getAttribute('data-source');
+    }
+    return;
+}
+
+function getCloseModal () {
+    lightbox.classList.remove('is-open');
+    ligthImage.src = '';
+}
+
+getElementByGallery(gallery, listGalleryRef);
+closeModal.addEventListener('click', getCloseModal);
+listGalleryRef.addEventListener('click', getOpenModale);
